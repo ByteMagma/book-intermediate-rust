234617c6bc7ea93f7a775381d701712b7cc20d6a
Gregory Lafrance <greglafrance007@gmail.com>
2025-06-08 06:00:47 -0400

    removed unnecessary placeholder files

--- exercises/chapter-1-placeholder/exercise_4_user_info_tuple/src/main.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let user_info = ("Frank Smith", 30, "Engineer", "fsmith@xyxwebsite.com");
-    println!("User name: {}", user_info.0);
-    println!("Age: {}", user_info.1);
-    println!("Occupation: {}", user_info.2);
-    println!("Email: {}", user_info.3);
-}
